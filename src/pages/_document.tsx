9beae15ef230ab215e649050af226a914e398fe3
jseabrook <jseabrook@hugoandcat.com>
2026-02-16 14:28:55 +0000

    Added app icons

---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Apple Touch Icons */}
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+        
+        {/* Android/Chrome Icons */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+        
+        {/* Favicons */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/icons/manifest.json" />
+        
+        {/* Microsoft Tiles */}
+        <meta name="msapplication-TileColor" content="#FF6B35" />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        
+        {/* Theme Color */}
+        <meta name="theme-color" content="#FF6B35" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
